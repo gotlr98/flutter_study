ad2b37a8c8b384c7c5c5ef961f5ccc5c6091aaca
gotlr98 <gotlr98@naver.com>
2023-09-21 16:35:48 +0900

    lecture ~ 3.0 function

--- test1/main.dart
+++ test1/main.dart
@@ -9,13 +9,9 @@ void main(){
   }
 
   String? sik = 'haesik';
-  sik = null;
+  // sik = null;
 
-  if(sik != null){
-    print(sik.length);
-  }
 
-  sik?.isNotEmpty;
 
   final y = 'w';
   final String x = 'aw';
@@ -26,4 +22,48 @@ void main(){
   const API = '1212123';
   // compile know value
 
+  num a = 12;
+  a = 1.1;
+
+  var giveMeFive = true;
+  var numbers = [1,2,3,4,
+  if (giveMeFive) 5];
+  // collection for
+
+  print(numbers);
+
+  var age = 10;
+  var greeting = 'Hello everyone I\'m $sik and ${age+2} years old';
+  print(greeting);
+
+  var oldFriends = ['sik', 'eee'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for(var friend in oldFriends) "with $friend",
+  ];
+  print(newFriends);
+
+  var player = {
+    'name': 'sik',
+    'xp': 19.99,
+    'superpower': false,
+  };
+
+  Map<int, bool> pla = {
+    1: true,
+    2: false,
+  };
+
+  // Set
+  var numbers2 = {1,2,3,4};
+
+  sayHello("sik");
 }
+
+void sayHello(String name){
+  print('Hello $name nice to meet you!');
+}
+
+String say(String potato) => "Hello $potato nice to meet you";
