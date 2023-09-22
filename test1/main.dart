889ccca5ad540119ae3bb6a85aec8c51cc9ab978
gotlr98 <gotlr98@naver.com>
2023-09-22 15:10:53 +0900

    feat: ~function

--- test1/main.dart
+++ test1/main.dart
@@ -1,69 +1,94 @@
-void main(){
-  dynamic name;
+// void main(){
+//   dynamic name;
   
-  if(name is String){
+//   if(name is String){
     
-  }
-  else if (name is int){
-    name.isEven;
-  }
+//   }
+//   else if (name is int){
+//     name.isEven;
+//   }
 
-  String? sik = 'haesik';
-  // sik = null;
+//   String? sik = 'haesik';
+//   // sik = null;
 
 
 
-  final y = 'w';
-  final String x = 'aw';
+//   final y = 'w';
+//   final String x = 'aw';
 
-  late final haesik;
-  // late -> without Initialize
+//   late final haesik;
+//   // late -> without Initialize
 
-  const API = '1212123';
-  // compile know value
+//   const API = '1212123';
+//   // compile know value
 
-  num a = 12;
-  a = 1.1;
+//   num a = 12;
+//   a = 1.1;
 
-  var giveMeFive = true;
-  var numbers = [1,2,3,4,
-  if (giveMeFive) 5];
-  // collection for
+//   var giveMeFive = true;
+//   var numbers = [1,2,3,4,
+//   if (giveMeFive) 5];
+//   // collection for
 
-  print(numbers);
+//   print(numbers);
 
-  var age = 10;
-  var greeting = 'Hello everyone I\'m $sik and ${age+2} years old';
-  print(greeting);
+//   var age = 10;
+//   var greeting = 'Hello everyone I\'m $sik and ${age+2} years old';
+//   print(greeting);
 
-  var oldFriends = ['sik', 'eee'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for(var friend in oldFriends) "with $friend",
-  ];
-  print(newFriends);
+//   var oldFriends = ['sik', 'eee'];
+//   var newFriends = [
+//     'lewis',
+//     'ralph',
+//     'darren',
+//     for(var friend in oldFriends) "with $friend",
+//   ];
+//   print(newFriends);
 
-  var player = {
-    'name': 'sik',
-    'xp': 19.99,
-    'superpower': false,
-  };
+//   var player = {
+//     'name': 'sik',
+//     'xp': 19.99,
+//     'superpower': false,
+//   };
 
-  Map<int, bool> pla = {
-    1: true,
-    2: false,
-  };
+//   Map<int, bool> pla = {
+//     1: true,
+//     2: false,
+//   };
 
-  // Set
-  var numbers2 = {1,2,3,4};
+//   // Set
+//   var numbers2 = {1,2,3,4};
 
-  sayHello("sik");
+//   sayHello("sik");
+// }
+
+// void sayHello(String name){
+//   print('Hello $name nice to meet you!');
+// }
+
+// String say(String potato) => "Hello $potato nice to meet you";
+
+// named parameters
+String namedHello({
+      required String name,
+      required int age,
+      required String country}){
+
+  return "Hello $name, you are $age, you from ";
 }
 
-void sayHello(String name){
-  print('Hello $name nice to meet you!');
+String sayHello({
+      String name = "anon",
+      int age = 0,
+      String country = "korea"}){
+
+  return "Hello $name, you are $age, you from ";
 }
 
-String say(String potato) => "Hello $potato nice to meet you";
+
+
+void main(){
+
+  print(namedHello(name: "sik", age: 21, country: "korea"));
+  print(sayHello());
+}
