8bcd378878fe10a81b2aa97e9662b218f96f8f7d
gotlr98 <gotlr98@naver.com>
2023-10-27 22:30:28 +0900

    re import to macbook

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -110,7 +110,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Episode(episode: episode)
+                          Episode(episode: episode, webtoon_id: ,)
                       ],
                     );
                   }
